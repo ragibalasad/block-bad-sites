abc2aa6a7c878752767c3ae73f6415ad792c49a1
Ragib Al Asad <ragibalasad@gmail.com>
2023-08-13 22:03:54 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Block-Bad-Sites
+Block Thousands of P*rn sites from your device within seconds.
+
+To do so, <b><a href="https://github.com/ragibalasad/Block-Bad-Sites/archive/refs/heads/main.zip"> download this repository</a></b> and run the <b>block.py</b> file.
+<h2>in Linux:</h2>
+Open terminal in the same directory where you downloaded and Type:<br>
+```$ sudo python3 block.py``` <br>
+Then hit Enter.<br><br>
+<h2>in Windows:</h2>
+<b>Run the python file as Administrator.</b>
---
+++ block.py
@@ -0,0 +1,24 @@
+import platform
+OS = platform.system()
+HOST = "127.0.0.1"
+HOSTS_PATH = "/etc/hosts" if OS == 'Linux' else "C:\Windows\System32\drivers\etc\hosts"
+
+print("Block Thousands of Popular P*rn Sites from your device with just one click.")
+confirm = input("Are you sure you want to proceed?? - [Y/n]")
+if confirm != "Y":
+    exit()
+else:
+    print("Note: You'll never be able to unblock these sites from this device unless you're a nerd.")
+    input("Hit ENTER ")
+
+with open("list.txt") as f:
+    haram_list = f.read()
+    haram_list = haram_list.split()
+
+    with open(HOSTS_PATH, "a") as f:
+        for i in haram_list:
+            print(f"Blocking {i}")
+            f.write(f"\n{HOST} {i}")
+            print("Done!")
+
+    print(f"Succesfully Blocked {len(haram_list)} site(s)!")
